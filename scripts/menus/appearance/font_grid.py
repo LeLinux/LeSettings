a361b220d5d66ccffb3f7c20673bfc3e667dda9a
ririkli <Ririk.li07@gmail.com>
2022-12-15 17:37:39 +0300

    font frontend commit

---
+++ scripts/menus/appearance/font_grid.py
@@ -0,0 +1,38 @@
+import gi
+gi.require_version("Gtk", "3.0")
+
+from gi.repository import Gtk as gtk
+
+from scripts.iconsNconst import *
+import scripts.go2 as g2
+import scripts.button_settings as btns
+import scripts.lesetlib as lsl
+
+mar_top, mar_start = inc.win_h / 24, inc.win_h / 3
+
+font_l = gtk.Label("Font")
+font_l.get_style_context().add_class("label")
+font_l.set_xalign(0)
+font_l.set_property("width-request", inc.main_const_def)
+font_l.set_margin_start(mar_start)
+font_l.set_margin_top(inc.win_h / 3.1)
+
+test_f_l = gtk.Label("Test text")
+test_f_l.get_style_context().add_class("label")
+test_f_l.set_property("width-request", inc.main_const_def)
+test_f_l.set_xalign(0)
+test_f_l.set_margin_start(mar_start)
+test_f_l.set_margin_top(mar_top)
+
+fonts_formats = ["Times new Roman", "Arial"]
+font_format_selector = gtk.ComboBoxText()
+for i in fonds_formats:
+    fond_format_selector.append_text(i)
+font_format_selector.set_margin_start(mar_start)
+font_format_selector.set_margin_top(mar_top)
+
+font_grid = gtk.Grid()
+font_grid.add(font_l)
+font_grid.attach(font_l, 1, 1, 0, 0)
+font_grid.attach_next_to(font_format_selector, font_l, gtk.PositionType.RIGHT, 1, 1)
+font_grid.attach_next_to(time_l, test_f_l, gtk.PositionType.BOTTOM, 1, 1)
